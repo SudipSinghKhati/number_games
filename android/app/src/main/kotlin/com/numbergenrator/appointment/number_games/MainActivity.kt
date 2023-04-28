5e9dfd8e70507d69acd45cf18d4dc795c1a85fc0
SudipSinghKhati <sudipkhati2043@gmail.com>
2023-04-28 15:50:56 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/numbergenrator/appointment/number_games/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.numbergenrator.appointment.number_games
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:number_games/app.dart';
+
+void main() {
+  runApp(const App());
+}
